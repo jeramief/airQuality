df1fc454a5a12a6fdf4d12e42833ce701f11d4b9
jeramief <109633173+jeramief@users.noreply.github.com>
2023-06-30 23:12:57 -0400

    air quality checker

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import CitySearch from "./components/CitySearch";
+import PollutantInfo from "./components/PollutantInfo";
+import AirQualityLevelsTable from "./components/AirQualityLevelsTable";
+import AirQualityCard from "./components/AirQualityCard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  const [airQualityData, setAirQualityData] = useState(null);
+  const [error, setError] = useState(null);
+  console.log();
+
+  const getAirQuality = async (city) => {
+    try {
+      const response = await fetch(
+        `https://api.waqi.info/feed/${city}/?token=${process.env.REACT_APP_AQI_API_TOKEN}`
+      );
+      const data = await response.json();
+      console.log(data);
+      if (response.ok && data.status === "ok") {
+        setAirQualityData(data.data);
+        setError(null);
+      } else {
+        setError(
+          "Sorry, we couldn't find the city you were looking for. Try another location nearby or ensure your spelling is correct."
+        );
+        setAirQualityData(null);
+      }
+    } catch (error) {
+      console.error("Network error:", error);
+      setError("Sorry, something went wrong. Please try again later.");
+      setAirQualityData(null);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="mt-5 mb-3">Air Quality Index Checker</h1>
+      <CitySearch getAirQuality={getAirQuality} />
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error}
+        </div>
+      )}
+      {airQualityData && (
+        <>
+          <AirQualityCard data={airQualityData} />
+          <PollutantInfo pollutant={airQualityData.dominentpol} />
+        </>
+      )}
+      <AirQualityLevelsTable />
+      <p>
+        Location-specific API data sourced from the World Air Quality Index
+        Project. <a href="https://aqicn.org/api/">LINK</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
